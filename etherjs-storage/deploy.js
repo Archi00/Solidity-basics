a2280a8cf09ed442f9c1ef4ef197164cbb80b71b
Archi00 <albert.archi@gmail.com>
2022-07-16 03:43:11 +0200

    Added fs-extra + provider + wallet wrapers using ethers

--- etherjs-storage/deploy.js
+++ etherjs-storage/deploy.js
@@ -1,5 +1,15 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
 async function main() {
   // http://127.0.0.1:7545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = ethers.Wallet(
+    "b2aa29989f2206160546cc61fc8ae8ed90249ace3f75a2fdff21de4083709a51",
+    provider
+  );
 }
 
 main()
