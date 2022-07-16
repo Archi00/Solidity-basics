97155f41216c2abe6b174019bded4ace81a09c10
Archi00 <albert.archi@gmail.com>
2022-07-16 23:36:46 +0200

    Added the verification function and the logic to verify the deployment of the contract in chainid 4 == goerli

--- hardhat-storage/scripts/deploy.js
+++ hardhat-storage/scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -8,9 +8,27 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed()
     console.log(`Depoyed contract to: ${simpleStorage.address}`)
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
 }
 
-async function verify(contractAddress, args) {}
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
 
 main()
     .then(() => process.exit(0))
