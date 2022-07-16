2a8f0be3e885a16a23f1fbaa50bb497fc9160911
Archi00 <albert.archi@gmail.com>
2022-07-16 04:31:09 +0200

    1st contract deployed to ganache

--- etherjs-storage/deploy.js
+++ etherjs-storage/deploy.js
@@ -14,9 +14,15 @@ async function main() {
   );
 
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait...");
+  console.log("Deploying, please wait...\n");
   const contract = await contractFactory.deploy();
-  console.log(contract);
+  await contract.deployTransaction.wait(1);
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
+  const transactionResponse = await contract.store("5");
+  const transactionReceipt = await transactionResponse.wait(1);
+  const updatedFavoriteNumber = await contract.retrieve();
+  console.log(`Updated favorite number is: ${updatedFavoriteNumber}`);
 }
 
 main()
