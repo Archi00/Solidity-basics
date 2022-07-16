a097cd5ac5103e82491691a3ac9db386ef11f913
Archi00 <albert.archi@gmail.com>
2022-07-16 03:53:28 +0200

    Fixed wallet issue and deployed 1st contract to ganache

--- etherjs-storage/deploy.js
+++ etherjs-storage/deploy.js
@@ -2,14 +2,21 @@ const ethers = require("ethers");
 const fs = require("fs-extra");
 
 async function main() {
-  // http://127.0.0.1:7545
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"
-  );
-  const wallet = ethers.Wallet(
+  const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:7545");
+  const wallet = new ethers.Wallet(
     "b2aa29989f2206160546cc61fc8ae8ed90249ace3f75a2fdff21de4083709a51",
     provider
   );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main()
