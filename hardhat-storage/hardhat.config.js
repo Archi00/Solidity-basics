c09d5d4c271e8c498d9ad51c429a1830abe94e45
Archi00 <albert.archi@gmail.com>
2022-07-16 23:11:48 +0200

    Added private keey + chainid

--- hardhat-storage/hardhat.config.js
+++ hardhat-storage/hardhat.config.js
@@ -4,11 +4,15 @@ require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         goerli: {
             url: GOERLI_RPC_URL,
+            account: [PRIVATE_KEY],
+            chainId: 5,
         },
     },
     solidity: "0.8.8",
