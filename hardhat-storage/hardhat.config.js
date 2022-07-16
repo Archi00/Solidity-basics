32f26c236ff9afe38662f0e81fd2345281aed5ae
Archi00 <albert.archi@gmail.com>
2022-07-16 22:32:37 +0200

    Added goerli network to hardhat config + .env file

--- hardhat-storage/hardhat.config.js
+++ hardhat-storage/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 module.exports = {
-  solidity: "0.8.8",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+        },
+    },
+    solidity: "0.8.8",
+}
--- hardhat-storage/scripts/deploy.js
+++ hardhat-storage/scripts/deploy.js
@@ -1,4 +1,14 @@
-async function main() {}
+const { ethers } = require("hardhat")
+
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Please wait, deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Depoyed contract to: ${simpleStorage.address}`)
+}
 
 main()
     .then(() => process.exit(0))
