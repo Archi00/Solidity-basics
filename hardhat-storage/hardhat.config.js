0a3bdc5b9cdc6c218887bf538837d8f15d37271a
Archi00 <albert.archi@gmail.com>
2022-07-17 04:00:56 +0200

    Added gas report dependencies plus config

--- hardhat-storage/hardhat.config.js
+++ hardhat-storage/hardhat.config.js
@@ -2,12 +2,14 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -25,5 +27,12 @@ module.exports = {
     solidity: "0.8.8",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        // coinmarketcap: ""
     },
 }
