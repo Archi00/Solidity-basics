ecc8fa6771da43873eecd57f03be520dd5bf937e
Archi00 <albert.archi@gmail.com>
2022-07-17 16:51:45 +0200

    Added checks for mocks and testnet deploys

--- hardhat-fundme/deploy/01-deploy-fund-me.ts
+++ hardhat-fundme/deploy/01-deploy-fund-me.ts
@@ -1,23 +1,26 @@
 import { DeployFunction } from "hardhat-deploy/dist/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { networkConfig } from "../helper-hardhat-config"
+import { devChains, networkConfig } from "../helper-hardhat-config"
 
 const deployFundme: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ) {
   const { getNamedAccounts, deployments, network } = hre
-  const { deploy, log } = deployments
+  const { deploy, log, get } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId: number = network.config.chainId!
 
-  const ethUsdPriceFeedAddress =
-    networkConfig[chainId]["ethUsdPriceFeedAddress"]
+  let ethUsdPriceFeedAddress: string
+  if (devChains.includes(network.name)) {
+    const ethUsdAggregator = await get("MockV3Aggregator")
+    ethUsdPriceFeedAddress = ethUsdAggregator.address
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeedAddress"]!
+  }
 
   const fundMe = await deploy("FundMe", {
     from: deployer,
-    args: [
-      /* Address */
-    ],
+    args: [ethUsdPriceFeedAddress],
     log: true,
   })
 }
