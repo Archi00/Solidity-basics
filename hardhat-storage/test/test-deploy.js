e82413e9971cb7c96e683233afe81dec194a5d74
Archi00 <albert.archi@gmail.com>
2022-07-17 03:45:29 +0200

    Added all tests

--- hardhat-storage/test/test-deploy.js
+++ hardhat-storage/test/test-deploy.js
@@ -13,4 +13,12 @@ describe("SimpleStorage", function () {
         const expectedValue = "0"
         assert.equal(currentValue.toString(), expectedValue)
     })
+    it("Should update when we call store", async function () {
+        const expectedValue = "7"
+        const transactionResponse = await simpleStorage.store(expectedValue)
+        await transactionResponse.wait(1)
+
+        const currentValue = await simpleStorage.retrieve()
+        assert.equal(currentValue.toString(), expectedValue)
+    })
 })
