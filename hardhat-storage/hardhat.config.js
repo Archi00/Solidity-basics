14bf5911709b7303b17898197c10380d816ea8ff
Archi00 <albert.archi@gmail.com>
2022-07-17 04:03:11 +0200

    Added failsafe in case env vars arent' defined

--- hardhat-storage/hardhat.config.js
+++ hardhat-storage/hardhat.config.js
@@ -6,10 +6,10 @@ require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 module.exports = {
     defaultNetwork: "hardhat",
