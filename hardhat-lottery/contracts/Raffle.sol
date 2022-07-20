57d8e6fd6ad137bf2bc94747fb0960c15cc67e6c
Archi00 <albert.archi@gmail.com>
2022-07-20 23:36:11 +0200

    Added basic functions to Raffle contract

--- hardhat-lottery/contracts/Raffle.sol
+++ hardhat-lottery/contracts/Raffle.sol
@@ -1,9 +1,29 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+error Raffle__NotEnoughEthEntered();
+
 contract Raffle {
-    
-    // function enterRaffe() public {}
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entraceFee) {
+        i_entranceFee = entraceFee;
+    }
+
+    function enterRaffe() public payable {
+        if (msg.value < i_entranceFee) { 
+            revert Raffle__NotEnoughEthEntered(); 
+            }
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayers(uint256 _index) public view returns (address) {
+        return s_players[_index];
+    }
 
     // function pickRandomWinner(){}
 }
