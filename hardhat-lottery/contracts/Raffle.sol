0ce20982b6d454c3e1bf5f3c9b81cfdbbd91f31a
Archi00 <albert.archi@gmail.com>
2022-07-21 04:19:59 +0200

    Added chainlink/contracts dependencies + imported VRFConsumerBaseV2

--- hardhat-lottery/contracts/Raffle.sol
+++ hardhat-lottery/contracts/Raffle.sol
@@ -1,9 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+
+/* Imports */
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+/* Custom errors */
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables  */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -11,7 +16,7 @@ contract Raffle {
     /* Events */
     event RaffleEnter(address indexed _player);
 
-    constructor(uint256 _entraceFee) {
+    constructor(address _vrfCoordinatorV2, uint256 _entraceFee) VRFConsumerBaseV2(_vrfCoordinatorV2) {
         i_entranceFee = _entraceFee;
     }
 
@@ -23,6 +28,13 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
+    function requestRandomWinner() external {
+
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
+
+    /* View / Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
@@ -31,5 +43,4 @@ contract Raffle {
         return s_players[_index];
     }
 
-    // function pickRandomWinner(){}
 }
