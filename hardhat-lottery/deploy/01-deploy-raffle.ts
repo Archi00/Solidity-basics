ae16f764e428083c6c6641f1feb68da7b8f6009b
Archi00 <albert.archi@gmail.com>
2022-07-25 19:21:08 +0200

    Added args for Raffle contract in deploy script

--- hardhat-lottery/deploy/01-deploy-raffle.ts
+++ hardhat-lottery/deploy/01-deploy-raffle.ts
@@ -7,21 +7,40 @@ import { DeployFunction } from "hardhat-deploy/types"
 import { ethers, network } from "hardhat"
 import { networkConfig } from "../helper-hardhat-config"
 
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
+
 const deployRaffle: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    let VRFCoordinatorV2Address
+    let vrfCoordinatorV2Address, subscriptionId
     log(network.name)
 
     if (devChains.includes(network.name)) {
-        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
-        VRFCoordinatorV2Address = VRFCoordinatorV2Mock.address
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const txResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await txResponse.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
     } else {
-        VRFCoordinatorV2Address = networkConfig[network.name]["vrfCoordinatorV2"]
+        vrfCoordinatorV2Address = networkConfig[network.name]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[network.name]["subscriptionId"]
     }
 
-    const args = [VRFCoordinatorV2Address]
+    const entranceFee = networkConfig[network.name]["entranceFee"]
+    const gasLane = networkConfig[network.name]["gasLane"]
+    const callbackGasLimit = networkConfig[network.name]["callbackGasLimit"]
+    const interval = networkConfig[network.name]["interval"]
+
+    const args = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args,
--- hardhat-lottery/helper-hardhat-config.ts
+++ hardhat-lottery/helper-hardhat-config.ts
@@ -1,11 +1,35 @@
+import { BigNumber } from "ethers"
+import { ethers } from "hardhat"
+
 export interface networkConfigItem {
     vrfCoordinatorV2?: string
+    entranceFee?: BigNumber
+    gasLane?: string
+    chainId?: number
+    subscriptionId?: string
+    callbackGasLimit?: string
+    interval?: string
 }
 export interface networkConfigInfo {
     [name: string]: networkConfigItem
 }
 export const networkConfig: networkConfigInfo = {
-    rinkeby: { vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab" },
+    hardhat: {
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        chainId: 31337,
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+    rinkeby: {
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        chainId: 4,
+        subscriptionId: "0",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
 }
 export const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 export const devChains = ["localhost", "hardhat"]
