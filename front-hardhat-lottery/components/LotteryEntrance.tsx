232fa76e24484d85935f8d9edbc49e18c6d3316f
Archi00 <albert.archi@gmail.com>
2022-07-28 04:45:42 +0200

    Added LotteryEntrance component

---
+++ front-hardhat-lottery/components/LotteryEntrance.tsx
@@ -0,0 +1,12 @@
+import { useWeb3Contract } from "react-moralis"
+
+export default function LotteryEntrance() {
+    // const {runContractFunction: enterRaffle} = useWeb3Contract({
+    //   abi:,
+    //   contractAddress: ,
+    //   functionName: "enterRaffle",
+    //   params: {},
+    //   msgValue:,
+    //   )
+    return <div></div>
+}
--- front-hardhat-lottery/pages/index.tsx
+++ front-hardhat-lottery/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Header from "../components/Header"
+import LotteryEntrance from "../components/LotteryEntrance"
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
+            <LotteryEntrance />
         </div>
     )
 }
