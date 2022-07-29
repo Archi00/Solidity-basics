4426d90b9cb8c6dcad4d5c69405beab897c616a5
Archi00 <albert.archi@gmail.com>
2022-07-29 17:31:26 +0200

    Added variables for Lottery entrance function

--- front-hardhat-lottery/components/LotteryEntrance.tsx
+++ front-hardhat-lottery/components/LotteryEntrance.tsx
@@ -1,12 +1,23 @@
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import {useMoralis} from "react-moralis"
+
+interface contractAddressesInterface{
+    [key:string]: string[]
+}
 
 export default function LotteryEntrance() {
-    // const {runContractFunction: enterRaffle} = useWeb3Contract({
-    //   abi:,
-    //   contractAddress: ,
-    //   functionName: "enterRaffle",
-    //   params: {},
-    //   msgValue:,
-    //   )
+    const addresses: contractAddressesInterface = contractAddresses
+    const {chainId:chainIdHex}=useMoralis()
+    const chainId:string = parseInt(chainIdHex!).toString()
+    const raffleAddress = chainId in addresses?addresses[chainId][0] : null
+    
+    const {runContractFunction: enterRaffle} = useWeb3Contract({
+      abi,
+      contractAddress:raffleAddress!,
+      functionName: "enterRaffle",
+      params: {},
+      msgValue:,
+      )
     return <div></div>
 }
