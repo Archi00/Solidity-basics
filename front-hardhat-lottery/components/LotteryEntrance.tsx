e7696e34169ff0db01298f9d5eb6f79e91e69506
Archi00 <albert.archi@gmail.com>
2022-07-31 16:11:20 +0200

    Added function calls + updateUI state

--- front-hardhat-lottery/components/LotteryEntrance.tsx
+++ front-hardhat-lottery/components/LotteryEntrance.tsx
@@ -16,6 +16,8 @@ export default function LotteryEntrance() {
     const raffleAddress = chainId in addresses ? addresses[chainId][0] : null
 
     const [entranceFee, setEntranceFee] = useState("0")
+    const [numPlayers, setNumPlayers] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
 
     const dispatch = useNotification()
 
@@ -32,10 +34,26 @@ export default function LotteryEntrance() {
         functionName: "getEntranceFee",
         params: {},
     })
+    const { runContractFunction: getNumOfPlayers } = useWeb3Contract({
+        abi,
+        contractAddress: raffleAddress!,
+        functionName: "getNumPlayers",
+        params: {},
+    })
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi,
+        contractAddress: raffleAddress!,
+        functionName: "getRecentWinner",
+        params: {},
+    })
 
     async function updateUI() {
         const entranceFeeFromCall = ((await getEntranceFee()) as BigNumber).toString()
+        const numPlayersFromCall = ((await getNumOfPlayers()) as BigNumber).toString()
+        const recentWinnerFromCall = (await getRecentWinner()) as string
         setEntranceFee(entranceFeeFromCall)
+        setNumPlayers(numPlayersFromCall)
+        setRecentWinner(recentWinnerFromCall)
     }
 
     useEffect(() => {
@@ -47,9 +65,18 @@ export default function LotteryEntrance() {
     const handleSuccess = async (tx: ContractTransaction) => {
         await tx.wait(1)
         handleNewNotification()
+        updateUI()
     }
 
-    const handleNewNotification = () => {}
+    const handleNewNotification = () => {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete!",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
 
     return (
         <div>
@@ -66,6 +93,8 @@ export default function LotteryEntrance() {
                         Enter Raffle
                     </button>
                     Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")}
+                    Players: {numPlayers}
+                    Recent Winner: {recentWinner}
                 </div>
             ) : (
                 <div>No Raffle Address Detected</div>
