6c4c7ce533e73ea7729f4e1ecbdb3e8e72839982
Archi00 <albert.archi@gmail.com>
2022-08-01 18:31:15 +0200

    First commit

---
+++ hardhat-erc20/hardhat.config.ts
@@ -0,0 +1,81 @@
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
+import "dotenv/config";
+import "solidity-coverage";
+import "hardhat-deploy";
+import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/config";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby";
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan";
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      from: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+      throwOnTransactionFailures: true,
+      loggingEnabled: true,
+    },
+    localhost: {
+      chainId: 31337,
+      throwOnTransactionFailures: true,
+      loggingEnabled: true,
+      throwOnCallFailures: true,
+      mining: {
+        auto: true,
+      },
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+      chainId: 4,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+    kovan: {
+      url: KOVAN_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 42,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "rinkeby",
+        chainId: 4,
+        urls: {
+          apiURL: "https://api-rinkeby.etherscan.io/api",
+          browserURL: "https://rinkeby.etherscan.io",
+        },
+      },
+    ],
+  },
+  namedAccounts: { deployer: { default: 0 } },
+  solidity: { compilers: [{ version: "0.8.8" }, { version: "0.4.24" }] },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  mocha: { timeout: 200000 },
+};
+export default config;
