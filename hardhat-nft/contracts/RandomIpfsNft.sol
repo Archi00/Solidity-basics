ff0550ff7ea6b5ca82019c3620982ec414a33661
Archi00 <albert.archi@gmail.com>
2022-08-02 05:41:40 +0200

    Added vrfConsumerBaseV2 + ERC721 contracts

--- hardhat-nft/contracts/RandomIpfsNft.sol
+++ hardhat-nft/contracts/RandomIpfsNft.sol
@@ -1,10 +1,52 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract RandomIpfsNft {
-    function requestNft() public {}
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {}
+    mapping(uint256 => address) public s_requestIdToSender;
 
-    function tokenURI(uint256) public {}
+    uint256 public s_tokenCounter;
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "rnd") {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function requestNft() public returns (uint256 requestId) {
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        s_requestIdToSender[requestId] = msg.sender;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {
+        address dogOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
+        _safeMint(dogOwner, newTokenId);
+    }
+
+    function tokenURI(uint256) public view override returns (string memory) {}
 }
