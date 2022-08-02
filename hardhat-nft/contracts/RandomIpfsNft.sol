5fe7b083b10156da79311bdadb6bc824230a0020
Archi00 <albert.archi@gmail.com>
2022-08-02 03:38:30 +0200

    Updated package.json

---
+++ hardhat-nft/contracts/RandomIpfsNft.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract RandomIpfsNft {
+    function requestNft() public {}
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {}
+
+    function tokenURI(uint256) public {}
+}
