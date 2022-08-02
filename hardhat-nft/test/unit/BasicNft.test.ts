bf2d86ac8f6147a734fa5bde7b6d57e578fb9e0b
Archi00 <albert.archi@gmail.com>
2022-08-02 03:15:39 +0200

    Updated basic tests for BasicNft contract

--- hardhat-nft/test/unit/BasicNft.test.ts
+++ hardhat-nft/test/unit/BasicNft.test.ts
@@ -7,6 +7,9 @@ import { assert } from "chai"
     : describe("Basic NFT Unit Tests", function () {
           let deployer: string
           let basicNft: BasicNft
+          const ipfs =
+              "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json"
+
           beforeEach(async function () {
               deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["basic"])
@@ -16,6 +19,19 @@ import { assert } from "chai"
               it("Initializes correctly", async () => {
                   const counter = (await basicNft.getTokenCounter()).toString()
                   assert.equal(counter, "0")
+              })
+          })
+          describe("mint", function () {
+              it("Counter gets updated", async () => {
+                  await basicNft._mintNft()
+                  const counter = (await basicNft.getTokenCounter()).toString()
+                  assert.equal(counter, "1")
+              })
+          })
+          describe("TOKEN_URI", function () {
+              it("returns the same ipfs CID", async () => {
+                  const getIpfs = await basicNft.tokenURI(0)
+                  assert.equal(getIpfs, ipfs)
               })
           })
       })
