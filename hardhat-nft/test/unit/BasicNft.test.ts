d115ba85eb19901f76b4f86b1a5ac7cbc7630166
Archi00 <albert.archi@gmail.com>
2022-08-02 03:26:47 +0200

    Updated tests

--- hardhat-nft/test/unit/BasicNft.test.ts
+++ hardhat-nft/test/unit/BasicNft.test.ts
@@ -7,14 +7,13 @@ import { assert } from "chai"
     : describe("Basic NFT Unit Tests", function () {
           let deployer: string
           let basicNft: BasicNft
-          const ipfs =
-              "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json"
 
           beforeEach(async function () {
               deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["basic"])
               basicNft = await ethers.getContract("BasicNft", deployer)
           })
+
           describe("constructor", function () {
               it("Initializes correctly", async () => {
                   const counter = (await basicNft.getTokenCounter()).toString()
@@ -23,15 +22,18 @@ import { assert } from "chai"
           })
           describe("mint", function () {
               it("Counter gets updated", async () => {
-                  await basicNft._mintNft()
+                  const tx = await basicNft._mintNft()
+                  await tx.wait(1)
                   const counter = (await basicNft.getTokenCounter()).toString()
                   assert.equal(counter, "1")
               })
           })
-          describe("TOKEN_URI", function () {
+          describe("token URI", function () {
               it("returns the same ipfs CID", async () => {
+                  const tx = await basicNft._mintNft()
+                  await tx.wait(1)
                   const getIpfs = await basicNft.tokenURI(0)
-                  assert.equal(getIpfs, ipfs)
+                  assert.equal(getIpfs, await basicNft.TOKEN_URI())
               })
           })
       })
