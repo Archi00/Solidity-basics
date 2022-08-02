a9fee8bd234046dd7994db9a640d9da124238462
Archi00 <albert.archi@gmail.com>
2022-08-02 02:37:05 +0200

    Updated hardhat.config.ts file

--- hardhat-lottery/hardhat.config.ts
+++ hardhat-lottery/hardhat.config.ts
@@ -9,73 +9,125 @@ import "hardhat-deploy"
 import "solidity-coverage"
 import { HardhatUserConfig } from "hardhat/config"
 
-/** @type import('hardhat/config').HardhatUserConfig */
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
-const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan"
-// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const RINKEBY_RPC_URL =
+    process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
+const POLYGON_MAINNET_RPC_URL =
+    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+// optional
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+const REPORT_GAS = process.env.REPORT_GAS || false
 
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
             chainId: 31337,
-            from: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
-            throwOnTransactionFailures: true,
-            loggingEnabled: true,
         },
         localhost: {
             chainId: 31337,
-            throwOnTransactionFailures: true,
-            loggingEnabled: true,
-            throwOnCallFailures: true,
-            mining: {
-                auto: true,
-            },
-        },
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            saveDeployments: true,
-            chainId: 4,
-        },
-        goerli: {
-            url: GOERLI_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            chainId: 5,
         },
         kovan: {
             url: KOVAN_RPC_URL,
-            accounts: [PRIVATE_KEY],
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //accounts: {
+            //     mnemonic: MNEMONIC,
+            // },
+            saveDeployments: true,
             chainId: 42,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 4,
+        },
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 1,
+        },
+        polygon: {
+            url: POLYGON_MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 137,
         },
     },
     etherscan: {
+        // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
         apiKey: {
             rinkeby: ETHERSCAN_API_KEY,
+            kovan: ETHERSCAN_API_KEY,
+            polygon: POLYGONSCAN_API_KEY,
         },
-        customChains: [
-            {
-                network: "rinkeby",
-                chainId: 4,
-                urls: {
-                    apiURL: "https://api-rinkeby.etherscan.io/api",
-                    browserURL: "https://rinkeby.etherscan.io",
-                },
-            },
-        ],
     },
-    namedAccounts: { deployer: { default: 0 } },
-    solidity: { compilers: [{ version: "0.8.8" }, { version: "0.4.24" }] },
     gasReporter: {
-        enabled: false,
+        //@ts-ignore
+        enabled: REPORT_GAS,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
         // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
-    mocha: { timeout: 200000 },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["Raffle"],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.4.19",
+            },
+            {
+                version: "0.4.24",
+            },
+            {
+                version: "0.6.6",
+            },
+            {
+                version: "0.6.12",
+            },
+            {
+                version: "0.8.7",
+            },
+        ],
+    },
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
+    },
 }
+
 export default config
